4ede3921e0363027d6a04022ba571e7f4c7e0242
TheConnMan <bcconn2112@gmail.com>
2017-05-28 12:27:22 -0400

    Add basic Publisher

--- ts/publisher/Publisher.ts
+++ ts/publisher/Publisher.ts
@@ -1,5 +1,37 @@
+import * as AWS from 'aws-sdk';
+
 class Publisher {
 
+  private registry: { [key:string]:string; } = {};
+
+  private snsClient: AWS.SNS = new AWS.SNS();
+
+  public register(topic: string, clazz: string): void {
+    this.registry[clazz] = topic;
+  }
+
+  public getTopic(object: any): string {
+    return this.registry[object.constructor.name];
+  }
+
+  public send(object: any): Promise<any> {
+    var topic = this.getTopic(object);
+    if (!topic) {
+      throw new Error('No topic found for ' + (object.constructor.name));
+    }
+    return new Promise((resolve, reject) => {
+      this.snsClient.publish({
+        Message: JSON.stringify(object),
+        TargetArn: topic
+      }, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      })
+    });
+  }
 }
 
 export default Publisher;
